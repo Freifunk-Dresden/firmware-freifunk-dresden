8304fdbbf17ba188c2419511f794df6464e5fa3b
Stephan <stephan@freifunk-dresden.de>
2020-02-06 10:53:59 +0000

    ddmesh-utils-wifi-info.shddmesh-utils-wifi-info.sh

--- files/common/usr/lib/ddmesh/ddmesh-utils-wifi-info.sh
+++ files/common/usr/lib/ddmesh/ddmesh-utils-wifi-info.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
-PRE="wifi_status"
-status="$(wifi status)"
 
-eval $(echo "$status" | jsonfilter -e "$PRE"_radio2g_up='@.radio2g.up' -e "$PRE"_radio5g_up='@.radio5g.up')
+_PRE="wifi_status"
+_status="$(wifi status)"
+echo $_status
+eval $(echo "$status" | jsonfilter \
+	-e "$PRE"_radio2g_up='@.radio2g.up' \
+	-e "$PRE"_radio5g_up='@.radio5g.up' \
+	-e _radio2g_ifname='@.radio2g.interfaces[0].ifname' \
+	-e _radio5g_ifname='@.radio5g.interfaces[0].ifname' \
+)
+
+echo $_radio2g_ifname
+echo $_radio5g_ifname
+
+eval "$PRE"_radio2g_phy=$(iwinfo $_radio2g_ifname info | sed -n '/PHY name:/{s#.*\(phy[0-9]\)$#\1#p}')
+eval "$PRE"_radio5g_phy=$(iwinfo $_radio5g_ifname info | sed -n '/PHY name:/{s#.*\(phy[0-9]\)$#\1#p}')
+
+unset _PRE
+unset _status
+unset _radio2g_ifname
+unset _radio5g_ifname
 
