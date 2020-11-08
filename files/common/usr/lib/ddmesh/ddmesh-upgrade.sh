6931e4c848e8bf043413db07778572d0f55317a6
Stephan <stephan@freifunk-dresden.de>
2020-11-08 20:53:41 +0100

    upgrade: add wg_port to ddmesh config

--- files/common/usr/lib/ddmesh/ddmesh-upgrade.sh
+++ files/common/usr/lib/ddmesh/ddmesh-upgrade.sh
@@ -467,6 +467,7 @@ upgrade_6_1_4()
  uci -q rename ddmesh.privnet.default_server_port='default_fastd_port'
  uci -q rename ddmesh.privnet.server_port='fastd_port'
  uci set ddmesh.backbone.default_wg_port='5003'
+ uci set ddmesh.backbone.wg_port='5003'
 }
 
 
