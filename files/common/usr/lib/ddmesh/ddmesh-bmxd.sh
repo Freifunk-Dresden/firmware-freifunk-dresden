123a9a1baca4c1f6e8a5b3648314c8a7b23f3717
Stephan <stephan@freifunk-dresden.de>
2020-11-16 21:39:58 +0100

    add network info cache and rework sysinfo.json generation (iptables-statistic)

--- files/common/usr/lib/ddmesh/ddmesh-bmxd.sh
+++ files/common/usr/lib/ddmesh/ddmesh-bmxd.sh
@@ -5,6 +5,9 @@ ARG2=$2
 
 DAEMON=bmxd
 DAEMON_PATH=/usr/bin
+
+test -x $DAEMON_PATH/$DAEMON || exit 0
+
 RUN_STATUS_FILE=/var/run/batman-status-running
 DB_PATH=/var/lib/ddmesh/bmxd
 STAT_DIR=/var/statistic
@@ -22,56 +25,61 @@ touch $STAT_DIR/gateway_usage
 
 eval $(/usr/lib/ddmesh/ddmesh-ipcalc.sh -n $(uci get ddmesh.system.node))
 
-ROUTING_CLASS="$(uci -q get ddmesh.bmxd.routing_class)"
-ROUTING_CLASS="${ROUTING_CLASS:-3}"
-ROUTING_CLASS="-r $ROUTING_CLASS --gateway_hysteresis 20"
+if [ "$ARG1" = "start" -o "$ARG1" = "no_gateway" ]; then
+	ROUTING_CLASS="$(uci -q get ddmesh.bmxd.routing_class)"
+	ROUTING_CLASS="${ROUTING_CLASS:-3}"
+	ROUTING_CLASS="-r $ROUTING_CLASS --gateway_hysteresis 20"
+fi
 
-GATEWAY_CLASS="$(uci -q get ddmesh.bmxd.gateway_class)"
-GATEWAY_CLASS="${GATEWAY_CLASS:-512/512}"
-GATEWAY_CLASS="-g $GATEWAY_CLASS"
+if [ "$ARG1" = "start" -o "$ARG1" = "gateway" ]; then
+	GATEWAY_CLASS="$(uci -q get ddmesh.bmxd.gateway_class)"
+	GATEWAY_CLASS="${GATEWAY_CLASS:-512/512}"
+	GATEWAY_CLASS="-g $GATEWAY_CLASS"
+fi
 
-MESH_NETWORK_ID="$(uci -q get ddmesh.network.mesh_network_id)"
-MESH_NETWORK_ID="${MESH_NETWORK_ID:-0}"
 
-PREFERRED_GATEWAY="$(uci -q get ddmesh.bmxd.preferred_gateway | sed -n '/^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$/p')"
-test -n "$PREFERRED_GATEWAY" && PREFERRED_GATEWAY="-p $PREFERRED_GATEWAY"
+case "$ARG1" in
 
-# create a virtual interface for primary interface. loopback has
-# 127er IP would be broadcasted
+  start)
+	MESH_NETWORK_ID="$(uci -q get ddmesh.network.mesh_network_id)"
+	MESH_NETWORK_ID="${MESH_NETWORK_ID:-0}"
 
-PRIMARY_IF="bmx_prime"
-FASTD_IF="tbb_fastd"
-LAN_IF="br-mesh_lan"
-WAN_IF="br-mesh_wan"
-if [ "$1" = "start" ]; then
-#	ip tuntap add dev $PRIMARY_IF mod tun
+	PREFERRED_GATEWAY="$(uci -q get ddmesh.bmxd.preferred_gateway | sed -n '/^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$/p')"
+	test -n "$PREFERRED_GATEWAY" && PREFERRED_GATEWAY="-p $PREFERRED_GATEWAY"
+
+	# create a virtual interface for primary interface. loopback has
+	# 127er IP which would be broadcasted
+
+	PRIMARY_IF="bmx_prime"
+	FASTD_IF="tbb_fastd"
+	LAN_IF="br-mesh_lan"
+	WAN_IF="br-mesh_wan"
+
 	brctl addbr $PRIMARY_IF
 	ip addr add $_ddmesh_ip/$_ddmesh_netpre broadcast $_ddmesh_broadcast dev $PRIMARY_IF
 	ip link set dev $PRIMARY_IF up
-fi
-_IF="dev=$PRIMARY_IF /linklayer 0 dev=$FASTD_IF /linklayer 1 dev=$LAN_IF /linklayer 1 dev=$WAN_IF /linklayer 1"
 
-#add wifi, if hotplug event did occur before starting bmxd
-eval $(/usr/lib/ddmesh/ddmesh-utils-network-info.sh wifi)
-if [ -n "$net_ifname" ]; then
-	_IF="$_IF dev=$net_ifname /linklayer 2"
-fi
+	_IF="dev=$PRIMARY_IF /linklayer 0 dev=$FASTD_IF /linklayer 1 dev=$LAN_IF /linklayer 1 dev=$WAN_IF /linklayer 1"
 
-#default start with no gatway.will be updated by gateway_check.sh
-#SPECIAL_OPTS="--throw-rules 0 --prio-rules 0 --meshNetworkIdPreferred $MESH_NETWORK_ID"
-SPECIAL_OPTS="--throw-rules 0 --prio-rules 0"
-TUNNEL_OPTS="--gateway_tunnel_network $_ddmesh_network/$_ddmesh_netpre --one-way-tunnel 1"
-TUNING_OPTS="--purge_timeout 20"
-DAEMON_OPTS="$SPECIAL_OPTS $TUNNEL_OPTS $TUNING_OPTS $ROUTING_CLASS $PREFERRED_GATEWAY $_IF"
+	#add wifi, if hotplug event did occur before starting bmxd
+	eval $(/usr/lib/ddmesh/ddmesh-utils-network-info.sh wifi)
+	if [ -n "$net_ifname" ]; then
+		_IF="$_IF dev=$net_ifname /linklayer 2"
+	fi
+
+	#default start with no gatway.will be updated by gateway_check.sh
+	#SPECIAL_OPTS="--throw-rules 0 --prio-rules 0 --meshNetworkIdPreferred $MESH_NETWORK_ID"
+	SPECIAL_OPTS="--throw-rules 0 --prio-rules 0"
+	TUNNEL_OPTS="--gateway_tunnel_network $_ddmesh_network/$_ddmesh_netpre --one-way-tunnel 1"
+	TUNING_OPTS="--purge_timeout 20"
+	DAEMON_OPTS="$SPECIAL_OPTS $TUNNEL_OPTS $TUNING_OPTS $ROUTING_CLASS $PREFERRED_GATEWAY $_IF"
 
 
-test -x $DAEMON_PATH/$DAEMON || exit 0
 
-case "$ARG1" in
-  start)
-	echo "Starting $DAEMON: opt: $DAEMON_OPTS"
 	# set initial wifi ssid to "FF no-inet"
 	/usr/lib/bmxd/bmxd-gateway.sh init
+
+	echo "Starting $DAEMON: opt: $DAEMON_OPTS"
 	$DAEMON_PATH/$DAEMON $DAEMON_OPTS
 	;;
 
