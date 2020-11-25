65b3959376689b911ce3916027c4f3b22e33547f
Niklas Merkelt <niklasmerkelt@mail.de>
2020-11-25 19:18:48 +0000

    Create version 6.1.5

--- files/common/usr/lib/ddmesh/ddmesh-upgrade.sh
+++ files/common/usr/lib/ddmesh/ddmesh-upgrade.sh
@@ -472,9 +472,13 @@ upgrade_6_1_4()
  uci add ddmesh log
  uci rename ddmesh.@log[-1]='log'
  uci -q set ddmesh.log.tasks=0
+}
+
+upgrade_6_1_5()
+{
  uci set ddmesh.network.wifi_channel_5g=44
- uci add ddmesh.network.wifi_indoor_5g=0
- uci add ddmesh.network.wifi_channels_5g_outdoor='100-140'
+ uci set ddmesh.network.wifi_indoor_5g=0
+ uci set ddmesh.network.wifi_channels_5g_outdoor='100-140'
 }
 
 
