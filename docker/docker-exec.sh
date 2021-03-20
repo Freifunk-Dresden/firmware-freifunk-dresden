cd57dc5f33c8558d1e94d49454a8a81332923bdf
Stephan <stephan@freifunk-dresden.de>
2021-03-20 17:05:22 +0000

    Update docker-exec.sh

--- docker/docker-exec.sh
+++ docker/docker-exec.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-# execute bash in running gitlab-runner container
+# execute bash in running gitlab-runner container.
+# gitlab-runner docker container was created with volume
+# "-v gitlab-runner-config:/etc/gitlab-runner".
+# this contains the gitlab-runner config: config.toml
+# see: https://docs.gitlab.com/runner/configuration/advanced-configuration.html
 
 docker exec -it gitlab-runner bash
