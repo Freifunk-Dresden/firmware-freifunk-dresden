7a380b7c15daed603bd7607825c4d0b190e6a595
Stephan <stephan@freifunk-dresden.de>
2021-05-02 13:29:09 +0200

    fix wifi_scan_fix

--- files/common/usr/lib/ddmesh/ddmesh-tasks.sh
+++ files/common/usr/lib/ddmesh/ddmesh-tasks.sh
@@ -50,8 +50,7 @@ call_task()
 # still needed in 7.0.2 (openwrt 1806, ar71xx TP-Link TL-WR1043N/ND )
 task_wifi_scanfix()
 {
-	eval $(/usr/lib/ddmesh/ddmesh-utils-network-info.sh wifi_adhoc)
-	/usr/sbin/iw dev $net_ifname scan >/dev/null
+	/usr/sbin/iw dev wifi2ap scan >/dev/null
 }
 
 task_routing()
@@ -82,7 +81,7 @@ do
 	# call_task <interval-minutes> <script | function> [arguments...]
 
 	call_task 3 task_routing
-	call_task 7 task_wifi_scanfix
+	call_task 10 task_wifi_scanfix
 	call_task 5 /usr/lib/ddmesh/ddmesh-backbone.sh runcheck
 	call_task 5 /usr/lib/ddmesh/ddmesh-privnet.sh runcheck
 	call_task 1 /usr/lib/ddmesh/ddmesh-sysinfo.sh
