a4372dbff9181040afebba6bb0a3d9cdd22752ab
Stephan Enderlein <stephan@freifunk-dresden.de>
2021-08-27 01:11:36 +0200

    hide splash config if not used (disabled)

--- files/common/www/admin/48-wifi-2g.sh
+++ files/common/www/admin/48-wifi-2g.sh
@@ -5,7 +5,13 @@ eval $(/usr/lib/ddmesh/ddmesh-utils-wifi-info.sh)
 if [ "$wifi_status_radio2g_up" = "1" ]; then
 cat<<EOM
 <tr><td><div class="plugin"><a class="plugin" href="wifi-2g.cgi">WiFi 2.4 GHz</a></div></td></tr>
+EOM
+
+# slash will be removed; keep it sill active if used
+if [ "$(uci -q get ddmesh.system.disable_splash)" != "1" ]; then
+cat<<EOM
 <tr><td><div class="plugin">&nbsp;&nbsp;<a class="plugin" href="splash.cgi">Splash</a></div></td></tr>
 EOM
 fi
 
+fi
