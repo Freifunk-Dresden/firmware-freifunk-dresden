1f7cf6f02043dc34f58a60f5e6891b19ac0f88e0
Stephan Enderlein <stephan@freifunk-dresden.de>
2021-10-14 15:06:56 +0200

    run-valgrind-bmxd.sh vlan 1

--- feeds/common/bmxd/run-valgrind-bmxd.sh
+++ feeds/common/bmxd/run-valgrind-bmxd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BMXD_DEBUG_LEVEL=0
+BMXD_DEBUG_LEVEL=4
 PrimeDEV="bmx_prime"
 PIP="10.200.99.99"
 LinkDEV="br-bmx0"				# empty bridge
@@ -19,7 +19,6 @@ usage()
 {
 	echo "Usage: $(basename) [server | client | setup-if | clean-if | bmxd]"
 	echo "server - run bmxd in forground (-d${BMXD_DEBUG_LEVEL})"
-	echo "client - run bmxd as client (-lcd${BMXD_DEBUG_LEVEL})"
 	echo "setup-if - only setup interfaces"
 	echo "clean-if - delete interfaces"
 	echo "bmxd     - calles bmxd and pass all other arguments to it"
@@ -27,6 +26,7 @@ usage()
 
 setup()
 {
+	# primary interface
 	ip link show dev ${PrimeDEV} || {
 		echo "create bmxd prime interface: ${PrimeDEV}: ${PIP}"
 		ip link add ${PrimeDEV} type bridge
@@ -34,11 +34,17 @@ setup()
 	}
 	ip link set ${PrimeDEV} up
 
+	# vlan 1
+	ip link add link ${LAN_DEV} ${LAN_DEV}.1 type vlan id 1
+	ip link set ${LAN_DEV}.1 up
+	brctl addif br-bmx0 ${LAN_DEV}.1
+
+
 	ip link show dev ${LinkDEV} || {
-		echo "create bmxd prime interface: ${LinkDEV}: ${LinkIP}"
+		echo "create bmxd link interface: ${LinkDEV}: ${LinkIP}"
 		ip link add ${LinkDEV} type bridge
 		ip addr add ${LinkIP}/16 broadcast ${BROADCAST} dev ${LinkDEV}
-		brctl addif ${LinkDEV} ${LAN_DEV}
+		brctl addif ${LinkDEV} ${LAN_DEV}.1
 	}
 	ip link set ${LinkDEV} up
 }
@@ -63,14 +69,11 @@ VALGRIND_OPT="--tool=memcheck --show-error-list=yes --leak-check=full -s --track
 case "$1" in
 	server)
 		setup
-		CMD="./sources/bmxd --fast_path_hysteresis 0 --path_hysteresis 10 -r3 -d${BMXD_DEBUG_LEVEL} dev=${PrimeDEV} /linklayer 0 dev=${LinkDEV} /linklayer 1"
+		CMD="./sources/bmxd --network 10.200.0.0/16 --netid 0 --throw-rules 0 --prio-rules 0 --gateway_tunnel_network 10.200.0.0/16 --gateway_hysteresis 20 --path_hysteresis 3  -r 3 --gateway_hysteresis 20 -p 10.200.1.2 --ogm_broadcasts 100 --udp_data_size 512 --ogm_interval 10000 --purge_timeout 20 -d${BMXD_DEBUG_LEVEL} dev=${PrimeDEV} /linklayer 0 dev=${LinkDEV} /linklayer 1"
 		echo "valgrind: [${VALGRIND_OPT}]"
 		echo "cmd: [${CMD}]"
 		valgrind ${VALGRIND_OPT} ${CMD}
 		clean
-		;;
-	client)
-		valgrind ${VALGRIND_OPT} ./sources/bmxd -lcd${BMXD_DEBUG_LEVEL}
 		;;
 	bmxd)
 		shift
