a399772a7094fb49797413d582a8b232d5a5fec1
Stephan Enderlein <stephan@freifunk-dresden.de>
2021-11-15 13:54:29 +0100

    tools: batch-cmd.sh

--- tools/batch-cmd.sh
+++ tools/batch-cmd.sh
@@ -3,6 +3,7 @@
 # simple template to do same job on different routers
 ROUTER="${ROUTER} 10.200.4.100" 
 ROUTER="${ROUTER} 10.200.4.177"
+ROUTER="${ROUTER} 10.200.19.162"
 ROUTER="${ROUTER} 10.200.5.222"
 ROUTER="${ROUTER} 10.200.19.156"
 ROUTER="${ROUTER} 10.200.19.160"
@@ -15,5 +16,10 @@ do
 	echo $i
 #	scp files/common/etc/config/firewall root@$i:/etc/config
 #	ssh -x root@$i "(sleep 20;reboot)&"
-	ssh -x root@$i "bmxd -c --status"
+	ssh -x root@$i "bmxd -c --status --links"
+#	ssh -x root@$i "uci set ddmesh.boot.boot_step=2; uci commit;"
+#	ssh -x root@$i "md5sum /rom/etc/config/firewall /etc/config/firewall"
+#	ssh -x root@$i "/usr/lib/ddmesh/ddmesh-overlay-md5sum.sh write"
+#	ssh -x root@$i "(sleep 20;reboot)&"
+	
 done
