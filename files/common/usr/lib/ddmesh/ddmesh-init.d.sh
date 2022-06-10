bf27d72120e7736102b6812b89c41f91c0eb37a3
Stephan <stephan@freifunk-dresden.de>
2022-06-10 20:46:17 +0000

    - rename post_init.sh to post-init.sh - provide post-init.sh because it would be insider knowledge to know about it

--- files/common/usr/lib/ddmesh/ddmesh-init.d.sh
+++ files/common/usr/lib/ddmesh/ddmesh-init.d.sh
@@ -108,9 +108,9 @@ start() {
 		/usr/lib/ddmesh/ddmesh-geoloc.sh mobile &
 	fi
 
-	if [ -x /etc/ddmesh/post_init.sh ]; then
-		logger -s -t $LOGGER_TAG "post init script"
-		/etc/ddmesh/post_init.sh
+	if [ -x /etc/ddmesh/post-init.sh ]; then
+		logger -s -t $LOGGER_TAG "call post init script"
+		/etc/ddmesh/post-init.sh
 	fi
 
 	logger -s -t $LOGGER_TAG "register node"
