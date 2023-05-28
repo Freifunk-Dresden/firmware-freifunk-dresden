240b876a3d75847507a19775bdc010645592b95c
Stephan Enderlein <stephan@freifunk-dresden.de>
2023-05-28 23:04:47 +0200

    display temp message to remind user that router is still turned on

--- files/common/usr/lib/ddmesh/ddmesh-display.sh
+++ files/common/usr/lib/ddmesh/ddmesh-display.sh
@@ -1,8 +1,8 @@
 #!/bin/ash
 
 if [ -z "$1" ]; then
-  echo "$(basename $0) [msg \"text\"] | update | boot | reboot | factory"
-  exit 1
+	echo "$(basename $0) [msg \"text\"] | update | boot | reboot | factory"
+	exit 1
 fi
 
 . /lib/functions.sh
@@ -17,111 +17,128 @@ eval $(/usr/lib/ddmesh/ddmesh-utils-network-info.sh lan lan)
 
 update()
 {
-  eval $(cat ${sysinfo} | jsonfilter \
-      -e gateway_ip='@.data.bmxd.gateways.selected' \
-      -e community='@.data.common.community' \
-      -e clients2g='@.data.statistic.client2g["15min"]' \
-      -e clients5g='@.data.statistic.client5g["15min"]' \
-        )
+	eval $(cat ${sysinfo} | jsonfilter \
+			-e gateway_ip='@.data.bmxd.gateways.selected' \
+			-e community='@.data.common.community' \
+			-e clients2g='@.data.statistic.client2g["15min"]' \
+			-e clients5g='@.data.statistic.client5g["15min"]' \
+				)
 
-  json="{ "
-  json="${json} \"ssid\": \"$(uci get wireless.wifi2_2g.ssid)\","
-  json="${json} \"up\": \"${wifi_status_radio2g_up}\","
-  json="${json} \"ssid_5g\": \"$(uci get wireless.wifi2_5g.ssid)\","
-  json="${json} \"up_5g\": \"${wifi_status_radio5g_up}\","
-  json="${json} \"key\": \"Free Wifi\","
-  json="${json} \"key_5g\": \"Free Wifi\","
+	json="{ "
+	json="${json} \"ssid\": \"$(uci get wireless.wifi2_2g.ssid)\","
+	json="${json} \"up\": \"${wifi_status_radio2g_up}\","
+	json="${json} \"ssid_5g\": \"$(uci get wireless.wifi2_5g.ssid)\","
+	json="${json} \"up_5g\": \"${wifi_status_radio5g_up}\","
+	json="${json} \"key\": \"Free Wifi\","
+	json="${json} \"key_5g\": \"Free Wifi\","
 
-  json="${json} \"work_mode\": \"Router\","
-  json="${json} \"lan_ip\": \"${lan_ipaddr}\","
+	json="${json} \"work_mode\": \"Router\","
+	json="${json} \"lan_ip\": \"${lan_ipaddr}\","
 
-  #get gateway
-  if [ -n "$gateway_ip" ]; then
-    json="${json} \"vpn_status\": \"connected\","
-    json="${json} \"vpn_server\": \"Node: $(/usr/lib/ddmesh/ddmesh-ipcalc.sh $gateway_ip)\","
-    #json="${json} \"vpn_type\": \"\","
+	#get gateway
+	if [ -n "$gateway_ip" ]; then
+		gw_node="$(/usr/lib/ddmesh/ddmesh-ipcalc.sh $gateway_ip)"
+		json="${json} \"vpn_status\": \"connected\","
+		json="${json} \"vpn_server\": \"Node: ${gw_node}\","
+		#json="${json} \"vpn_type\": \"\","
 
-    json="${json} \"method_nw\": \"modem\","	# removes "No Internet"
-  else
-    json="${json} \"vpn_status\": \"connecting\","  |
-    json="${json} \"vpn_server\": \"local/none\","
-    json="${json} \"method_nw\": \"\","	# "No Internet"
-  fi
+		json="${json} \"method_nw\": \"modem\","	# removes "No Internet"
+	else
+		gw_node="local/none"
+		json="${json} \"vpn_status\": \"connecting\","  |
+		json="${json} \"vpn_server\": \"${gw_node}\","
+		json="${json} \"method_nw\": \"\","	# "No Internet"
+	fi
 
-  json="${json} \"clients\": \"$(( $clients2g + $clients5g ))\","
-  json="${json} \"clock\": \"$(date +"%H:%M")\","
+	json="${json} \"clients\": \"$(( $clients2g + $clients5g ))\","
+	json="${json} \"clock\": \"$(date +"%H:%M")\","
 
-  # read lte status
-  lte_info_dir="/var/lib/ddmesh"
-  lte_info="$lte_info_dir/lte_info"
-  signal=0
-  if [ -f "$lte_info" ]; then
-    eval $(cat $lte_info | jsonfilter -e m_type='@.signal.type' \
-            -e m_rssi='@.signal.rssi' -e m_rsrq='@.signal.rsrq' \
-            -e m_rsrp='@.signal.rsrp' -e m_snr='@.signal.snr' \
-            -e m_conn='@.status' -e m_reg='@.registration')
+	# read lte status
+	lte_info_dir="/var/lib/ddmesh"
+	lte_info="$lte_info_dir/lte_info"
+	signal=0
+	if [ -f "$lte_info" ]; then
+		eval $(cat $lte_info | jsonfilter -e m_type='@.signal.type' \
+						-e m_rssi='@.signal.rssi' -e m_rsrq='@.signal.rsrq' \
+						-e m_rsrp='@.signal.rsrp' -e m_snr='@.signal.snr' \
+						-e m_conn='@.status' -e m_reg='@.registration')
 
 
-    case "$m_type" in
-      lte)    m_type="4G" ;;
-      wcdma)  m_type="3G" ;;
-      gsm)    m_type="2G" ;;
-      *)  m_type="" ;;
-    esac
-    json="${json} \"modem_mode\": \"$m_type\","
+		case "$m_type" in
+			lte)    m_type="4G" ;;
+			wcdma)  m_type="3G" ;;
+			gsm)    m_type="2G" ;;
+			*)  m_type="" ;;
+		esac
+		json="${json} \"modem_mode\": \"$m_type\","
 
-    signal=4
-    test $m_rssi -lt -60 && signal=3
-    test $m_rssi -lt -70 && signal=2
-    test $m_rssi -lt -80 && signal=1
-    test $m_rssi -lt -90 && signal=0
-    json="${json} \"signal\": \"${signal}\","
+		signal=4
+		test $m_rssi -lt -60 && signal=3
+		test $m_rssi -lt -70 && signal=2
+		test $m_rssi -lt -80 && signal=1
+		test $m_rssi -lt -90 && signal=0
+		json="${json} \"signal\": \"${signal}\","
 
-    if [ "$m_conn" = "connected" ]; then
-      json="${json} \"modem_up\": \"1\","
-    else
-      json="${json} \"modem_up\": \"0\","
-    fi
+		if [ "$m_conn" = "connected" ]; then
+			json="${json} \"modem_up\": \"1\","
+		else
+			json="${json} \"modem_up\": \"0\","
+		fi
 
-    case "$m_reg" in
-      registered) sim="0" ;;
-      searching) sim="NO_REG" ;;
-      *) sim="NO_SIM" ;;
-    esac
-    json="${json} \"SIM\": \"${sim}\","
+		case "$m_reg" in
+			registered) sim="0" ;;
+			searching) sim="NO_REG" ;;
+			*) sim="NO_SIM" ;;
+		esac
+		json="${json} \"SIM\": \"${sim}\","
 
-  else
-    json="${json} \"modem_up\": \"0\","
-    json="${json} \"SIM\": \"NO_SIM\","
-  fi
+	else
+		json="${json} \"modem_up\": \"0\","
+		json="${json} \"SIM\": \"NO_SIM\","
+	fi
 
-  json="${json} \"carrier\": \"$(printf '%.16s' "Freifunk ${community}")\","
+	json="${json} \"carrier\": \"$(printf '%.16s' "Freifunk ${community}")\","
 
-  #json="${json} \"mcu_status\": \"1\""
-  json="${json} }"
-  # echo "${json}" > ${TTY} ; cat ${TTY}
-  echo "${json}" > ${TTY}
+	#json="${json} \"mcu_status\": \"1\""
+	json="${json} }"
+	echo "${json}" > ${TTY}
+
+
+	# custom screen
+	L0left="Freifunk"
+	L0right="$(uci get ddmesh.system.node)"
+	L1left="${community}"
+	L1right=""
+	L2left="Clients:"
+	L2right="$(( $clients2g + $clients5g ))"
+	L3left="Gateway:"
+	L3right="${gw_node}"
+	m="$(printf "%-10.10s %5.5s%-10.10s %5.5s%-10.10s %5.5s%-10.10s %5.5s" \
+				"${L0left}" "${L0right}"  "${L1left}" "${L1right}" "${L2left}" "${L2right}" "${L3left}" "${L3right}")"
+
+	##reminder device is running
+	msg "${m}"
 }
 
 msg()
 {
-  message="$1"
-  json="{ \"msg\": \"$(printf "%.64s" "${message}")\" }"
-  echo "${json}" > ${TTY}
+	message="$1"
+	json="{ \"msg\": \"$(printf "%.64s" "${message}")\" }"
+	echo "${json}" > ${TTY}
 }
 
 system()
 {
-  mode="$1"
-  json="{ \"system\": \"${mode}\" }"
-  echo "${json}" > ${TTY}
+	mode="$1"
+	json="{ \"system\": \"${mode}\" }"
+	echo "${json}" > ${TTY}
 }
 
 case "$1" in
-  msg) msg "$2" ;;
-  update) update ;;
-  boot) system "boot" ;;
-  reboot) system "reboot" ;;
-  factory) system "reft" ;;
-  *) echo "invalid command" ;;
+	msg)			msg "$2" ;;
+	update)		update ;;
+	boot)			system "boot" ;;
+	reboot)		system "reboot" ;;
+	factory)	system "reft" ;;
+	*) echo "invalid command" ;;
 esac
