edcaf7dd0aa85afadb7b41c8794e5ca7f664b5ee
Stephan Enderlein <stephan@freifunk-dresden.de>
2023-07-23 03:21:38 +0200

    wifi: use correct flag to enable wifi2g gui

--- files/common/www/admin/48-wifi-2g.sh
+++ files/common/www/admin/48-wifi-2g.sh
@@ -2,7 +2,7 @@
 # Copyright (C) 2006 - present, Stephan Enderlein<stephan@freifunk-dresden.de>
 # GNU General Public License Version 3
 
-if [ "$wifi_status_radio2g_up" = "1" ]; then
+if [ "$wifi_status_radio2g_present" = "1" ]; then
 cat<<EOM
 <tr><td><div class="plugin"><a class="plugin" href="wifi-2g.cgi">WiFi 2.4 GHz</a></div></td></tr>
 EOM
