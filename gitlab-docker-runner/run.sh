4eda3d33417cbf6a7365e46206d50bf1bc28b52c
Stephan <stephan@freifunk-dresden.de>
2023-08-09 00:29:00 +0000

    docker stuff

--- gitlab-docker-runner/run.sh
+++ gitlab-docker-runner/run.sh
@@ -3,6 +3,6 @@
 # uses a volume to keep the build dir
 
 echo "using volume: openwrt-docker-build for builds"
-docker run --name test-container --rm -it -v openwrt-docker-build:/builds openwrt-docker-build /bin/bash
+docker run --user "$(id -u):$(id -g)" --name test-container --rm -it -v openwrt-docker-build:/builds openwrt-docker-build /bin/bash
 
 
